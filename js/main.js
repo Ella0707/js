d9ae3845c66b54145551c7c301bbc5fb2bee685c
Ella0707 <pakelvi77@gmail.com>
2022-05-09 23:14:29 +0300

    lern

---
+++ js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(myName); //всплытие переменных (hoisting) (нелогичное поведение,
+                    // undefined, до объявления переменной)
+var myName = 'Ella';
+
+
+{
+    let result = 50;
+}
+
+// console.log(result);
