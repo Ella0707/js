3c06ef1fcfe7412aec1a833b611618c615ceb081
Ella0707 <pakelvi77@gmail.com>
2022-05-28 23:03:01 +0300

    learn

--- js/main.js
+++ js/main.js
@@ -24,3 +24,33 @@ var myName = 'Ella';
 }
 
 // console.log(result);
+
+
+let inputIn = document.querySelector(".input-in");
+let myButton = document.querySelector("button");
+let out = document.querySelector(".out");
+
+myButton.onclick = function () {
+    // console.log(+inputIn.value * 10);
+    let inputValue = +inputIn.value
+    inputIn.value = "";
+    out.innerHTML = inputValue + 10;
+
+}
+
+let userName = document.querySelector(".name");
+let myButton2 = document.querySelector(".greetings");
+let surname = document.querySelector(".surname");
+let out2 = document.querySelector(".out2");
+surname.value = 'Tenor';
+
+
+myButton2.onclick = function() {
+    let nameValue = userName.value;
+    let surnameValue = surname.value;
+
+
+    out2.innerHTML = ('Hello' + ' ' + nameValue + ' ' + surnameValue);
+    userName.value = '';
+    surname.value = '';
+}
